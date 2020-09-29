a7864234dc32ea9c6df35208f41dc0d20e60dcb0
Rubemaster <ruben.rick@outlook.com>
2020-09-29 20:51:24 +0200

    Update PageElements.js

--- src/components/PageElements.js
+++ src/components/PageElements.js
@@ -1,2 +1,3 @@
 export {LocalNavbar as Navbar} from './Elements/Navbar.js';
 export {LocalBanner as Banner} from './Elements/Banner.js';
+export {LocalLinkRow as LinkRow} from './Elements/LinkRow.js';
