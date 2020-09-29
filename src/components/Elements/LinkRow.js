6cffd8fb5110c62e586db0e779be253c28f46997
Rubemaster <ruben.rick@outlook.com>
2020-09-29 21:00:22 +0200

    Wrapped return in LinkRow.js to avoid build fail

--- src/components/Elements/LinkRow.js
+++ src/components/Elements/LinkRow.js
@@ -3,9 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export function LocalLinkRow(props) {
 
-    return 
+    return (
         <div>
             <div class="col-10 border border-dark">First, but last</div>
             <div class="col border border-dark" style={{minWidth: "400px"}}>Second, but unordered</div>
-        </div>;
+        </div);
 }
