8e5a8e8339d90abb38ae3fdf7250a24317c8547c
Rubemaster <ruben.rick@outlook.com>
2020-09-29 20:55:09 +0200

    Update LinkRow.js

--- src/components/Elements/LinkRow.js
+++ src/components/Elements/LinkRow.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export function LocalLinkRow(props) {
 
-    return <div class="col-10 border border-dark">First, but last</div>
-        <div class="col border border-dark" style={{minWidth: "400px"}}>Second, but unordered</div>;
+    return (
+        <div class="col-10 border border-dark">First, but last</div>
+        <div class="col border border-dark" style={{minWidth: "400px"}}>Second, but unordered</div>);
 }
