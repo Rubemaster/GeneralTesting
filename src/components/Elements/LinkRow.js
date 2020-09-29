d350e10cb4b1eafbd00033ab7bd7024f612702bc
Rubemaster <ruben.rick@outlook.com>
2020-09-29 20:52:02 +0200

    Update LinkRow.js

--- src/components/Elements/LinkRow.js
+++ src/components/Elements/LinkRow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export function LocalBanner(props) {
+export function LocalLinkRow(props) {
 
     return <div class="col-10 border border-dark">First, but last</div>
         <div class="col border border-dark" style={{minWidth: "400px"}}>Second, but unordered</div>;
